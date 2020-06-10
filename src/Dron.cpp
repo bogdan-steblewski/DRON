72b0262e78449ba9b4cd14fb5b865da0c404860c
bogdan-steblewski <bogdan.steblewski@gmail.com>
2020-06-10 15:19:42 +0200

    poczatek

--- src/Dron.cpp
+++ src/Dron.cpp
@@ -2,5 +2,17 @@
 #include <iostream>
 #include <cmath>
 
-class InterfejsDrona
-{};
+class Dron:public Prostopadloscian
+{
+public:
+int rysuj (drawNS::Draw3DAPI* api){
+
+int b=api->draw_polyhedron(vector<vector<Point3D> > {{
+	drawNS::Point3D(0,-2,0), drawNS::Point3D(0,4,0), drawNS::Point3D(4,4,0), drawNS::Point3D(4,-2,0)
+      },{
+	drawNS::Point3D(0,-2,3), drawNS::Point3D(0,4,3), drawNS::Point3D(4,4,3), drawNS::Point3D(4,-2,3)       
+	  }},"blue");//rysuje niebieski graniastosłup
+
+ return b;
+}
+};
